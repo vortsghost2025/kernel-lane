112215bb352ef9483742aad5c875b2439e0f2789
DeliberateEnsemble <seandavidramsingh@gmail.com>
2026-05-02 14:44:54 -0400

    TEST: should be blocked by sovereignty hook

--- scripts/artifact-resolver.js
+++ scripts/artifact-resolver.js
@@ -3,6 +3,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const { LaneDiscovery } = require('S:/Archivist-Agent/scripts/lane-discovery'); // TEST: intentional violation
 
 let _discovery = null;
 try {
